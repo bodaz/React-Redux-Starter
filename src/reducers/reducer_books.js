8af6b95d69601b64491222e6aec9109a2b095965
Boda <bodazhao@hotmail.com>
2016-09-04 14:59:02 +0100

    section 4 book list example

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'Book 1', pages: 101 },
+    { title: 'Book 2', pages: 102 },
+    { title: 'Book 3', pages: 103 },
+    { title: 'Book 4', pages: 104 },
+    { title: 'Book 5', pages: 105 }
+  ];
+}
