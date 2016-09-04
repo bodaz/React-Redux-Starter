3410626d2a15124494a11c866d30943ac12869da
Boda <bodazhao@hotmail.com>
2016-09-04 15:17:48 +0100

    initial app boilerplate

--- src/reducers/reducer_books.js
+++
@@ -1,9 +0,0 @@
-export default function() {
-  return [
-    { title: 'Book 1', pages: 101 },
-    { title: 'Book 2', pages: 102 },
-    { title: 'Book 3', pages: 103 },
-    { title: 'Book 4', pages: 104 },
-    { title: 'Book 5', pages: 105 }
-  ];
-}
