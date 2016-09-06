fdd3f8129db9d0c43a57b2710e32da89e1bd2be1
Boda <bodazhao@hotmail.com>
2016-09-06 21:09:44 +0100

    update comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 // actions/index.js
 // contains all of our action curators
 
+// since we want to export multiple action creators from a single action creator file
+// we'll use export function rather than export default
 export function selectBook(book) {
   // console.log('a book has been selected: ', book.title);
   // selectBook is an ActionCreator, it needs to return an action
